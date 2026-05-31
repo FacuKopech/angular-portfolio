a61559c8c9f380a87d02ece187774d51bc12c955
Facu <facukopech@gmail.com>
2026-05-31 11:24:01 -0300

    improved logic to calculate years of experience and age

--- src/app/components/about/about/about.component.ts
+++ src/app/components/about/about/about.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
+  getAge(): number {
+    const birthDate = new Date(1999, 7, 27);
+    const today = new Date();
 
+    let age = today.getFullYear() - birthDate.getFullYear();
+
+    const hasHadBirthdayThisYear =
+      today.getMonth() > birthDate.getMonth() ||
+      (today.getMonth() === birthDate.getMonth() &&
+        today.getDate() >= birthDate.getDate());
+
+    if (!hasHadBirthdayThisYear) {
+      age--;
+    }
+
+    return age;
+  }
 }
